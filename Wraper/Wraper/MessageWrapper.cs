c38af4b545a0f463520083003eff835b75540c5d
Nireus <nir3us@gmail.com>
2012-12-13 02:31:38 +0100

    wrapper

---
+++ Wraper/Wraper/MessageWrapper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wraper
+{
+    class MessageWrapper
+    {
+        private String audioMsg;
+        private String videoMsg;
+        private String chatMsg;
+        private String wrappingMsg;
+        private Util util;
+
+        public MessageWrapper() {
+            util = new Util();
+            wrappingMsg = "";
+            audioMsg = "";
+            videoMsg = "";
+            chatMsg = "";
+        }
+
+        public void SetAudioMsg(string audio) {
+            this.audioMsg = audio;
+        }
+
+        public String GetAudioMsg () { 
+            return this.audioMsg;
+        }
+
+        public void SetVideoMsg(string video)
+        {
+            this.videoMsg = video;
+        }
+
+        public String GetVideoMsg()
+        {
+            return this.videoMsg;
+        }
+        public void SetChatMsg(string chat)
+        {
+            this.chatMsg = chat;
+        }
+
+        public String GetChatMsg()
+        {
+            return this.chatMsg;
+        }
+
+        public String Wrap() {
+            wrappingMsg = "";
+            wrappingMsg += util.WrapWithTag(audioMsg, Constants.otAudioMsg, Constants.ctAudioMsg);
+            wrappingMsg += util.WrapWithTag(videoMsg, Constants.otVideoMsg, Constants.ctVideoMsg);
+            wrappingMsg += util.WrapWithTag(chatMsg, Constants.otChatMsg, Constants.ctChatMsg);
+            return wrappingMsg;
+        }
+
+        public void Unwrap(String toUnwrap)
+        {
+            audioMsg = util.UnwrapTags(toUnwrap, Constants.otAudioMsg, Constants.ctAudioMsg);
+            videoMsg = util.UnwrapTags(toUnwrap, Constants.otVideoMsg, Constants.ctVideoMsg);
+            chatMsg = util.UnwrapTags(toUnwrap, Constants.otChatMsg, Constants.ctChatMsg);
+        }
+        
+    }
+}
---
+++ Wraper/Wraper/Util.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wraper
+{
+    class Util
+    {
+        private String tmp;
+
+
+        public String WrapWithTag(String msg, String openTag, String closeTag) {
+            return openTag + msg + closeTag; 
+        }
+
+        public String UnwrapTags(String toUnwrap, String openTag, String closeTag) {
+            int i,j;
+            Boolean found=false;
+            
+            int startIndex, endIndex;
+            int size = toUnwrap.Length;
+            tmp = "";
+            i = j = 0;
+            if (toUnwrap.Contains(openTag) == false)
+            {
+                return "";
+            }
+            for (i = 0, found = false; i < size && !found; i++)
+            {
+                for (j = 0; i + j < toUnwrap.Length && j< openTag.Length && toUnwrap.ElementAt(i + j) == openTag.ElementAt(j); j++)
+                {
+                    //Console.Write(toUnwrap.ElementAt(i + j));
+                   if (j == (openTag.Length - 1)) 
+                    {
+                        found = true;
+                    }
+                }
+                
+            }
+            startIndex = i + j -1;
+            //Console.WriteLine();
+            for (i = 0, found = false; i < size && !found; i++)
+            {
+                for (j = 0; i + j < toUnwrap.Length && j < closeTag.Length && toUnwrap.ElementAt(i + j) == closeTag.ElementAt(j); j++)
+                {
+                    //Console.Write(toUnwrap.ElementAt(i+j));
+                    if (j == (closeTag.Length - 1))
+                    {
+                        found = true;
+                    }
+
+                }
+                
+            }
+            //Console.WriteLine();
+            endIndex = i-1 ;
+            for (i = startIndex; i < endIndex; i++)
+            {
+                tmp = tmp + toUnwrap.ElementAt(i);
+               // Console.WriteLine(tmp);
+            }
+            //Console.WriteLine();
+                return tmp;
+        }
+    }
+}
