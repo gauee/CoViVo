9edc54b857bec213854e30792d9f68e3dac1fd15
Nireus <nir3us@gmail.com>
2012-12-27 19:13:43 +0100

    wesolych swiat

--- WrapperLib/WrapperLib/AudioVideoText.cs
+++ WrapperLib/WrapperLib/AudioVideoText.cs
@@ -8,6 +8,7 @@ using System.Xml.Serialization;
 
 namespace WrapperLib
 {
+    [Serializable]
     class AudioVideoText : Message
     {
         public byte[] audio
@@ -24,36 +25,5 @@ namespace WrapperLib
             
         }
 
-        public byte[] Wrap()
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(AudioVideoText));
-            StringBuilder sb = new StringBuilder();
-            TextWriter textWriter = new StringWriter(sb);
-            serializer.Serialize(textWriter, this);
-            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
-            byte[] tab = new byte[sb.Length - 1];
-            tab = encoding.GetBytes(sb.ToString());
-            textWriter.Close();
-            return tab;
-        }
-
-        public void Unwrap(byte[] msg)
-        {
-            XmlSerializer deserializer = new XmlSerializer(typeof(AudioVideoText));
-            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
-
-            StringBuilder sb = new StringBuilder();
-            TextReader textReader = new StringReader(encoding.GetString(msg));
-            AudioVideoText avt;
-            avt = (AudioVideoText)deserializer.Deserialize(textReader);
-            messageCode = avt.messageCode;
-            messageNumber = avt.messageNumber;
-            user = avt.user;
-            recipient = avt.recipient;
-            audio = avt.audio;
-            video = avt.video;
-            text = avt.text;
-
-        }
     }
 }
--- WrapperLib/WrapperLib/Message.cs
+++ WrapperLib/WrapperLib/Message.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace WrapperLib
-{
+{   
+    [Serializable]
+    [XmlInclude(typeof(AudioVideoText))]
+    [XmlInclude(typeof(ServerMsg))]
+    [XmlInclude(typeof(Error))]
     abstract class Message
     {
         public int messageCode
@@ -21,38 +25,12 @@ namespace WrapperLib
 
         public string recipient
         { get; set; }
-        public Message() {
+        public Message()
+        {
             messageCode = 0;
             messageNumber = 0;
             user = "";
             recipient = "";
-        }
-
-        public virtual byte[] Wrap() {
-            XmlSerializer serializer = new XmlSerializer(typeof(Message));
-            StringBuilder sb = new StringBuilder();
-            TextWriter textWriter = new StringWriter(sb);
-            serializer.Serialize(textWriter,this);
-            System.Text.ASCIIEncoding  encoding=new System.Text.ASCIIEncoding();
-            byte[] tab = new byte[sb.Length - 1];
-                tab = encoding.GetBytes(sb.ToString());
-            textWriter.Close();
-            return tab;
-        }
-
-        public virtual void Unwrap(byte[] msg) {
-            XmlSerializer deserializer = new XmlSerializer(typeof(Message));
-            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
-
-            StringBuilder sb = new StringBuilder();
-            TextReader textReader = new StringReader(encoding.GetString(msg));
-            Message kom;
-            kom = (Message)deserializer.Deserialize(textReader);
-            
-            messageCode = kom.messageCode;
-            messageNumber = kom.messageNumber;
-            user = kom.user;
-            recipient = kom.recipient;
         }
 
     }
