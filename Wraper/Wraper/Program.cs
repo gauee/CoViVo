ad73ee6fe8089a00ecb6833e1bf1b6af898a61b7
zglicz <michal.zgliczynski@uj.edu.pl>
2013-01-09 14:30:13 +0100

    Zmiana testu w Wraper. Poprawic .gitignore

--- Wraper/Wraper/Program.cs
+++ Wraper/Wraper/Program.cs
@@ -15,18 +15,25 @@ namespace Wraper
             avt = new Text();
             avt.messageCode = 5;
             avt.messageNumber = 1000;
-            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
 
             avt.text = "kuba";
 
             byte[] b = Util.Wrap(avt);
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
             Console.WriteLine(encoding.GetString(b));
-            Text at = (Text)Util.Unwrap(b);
-            Console.WriteLine(at.messageCode);
-            Console.WriteLine(at.messageNumber);
-            Console.WriteLine(at.text);
-            Console.WriteLine();
-            Console.ReadKey();
+            Message msg = Util.Unwrap(b);
+            if (msg is Text)
+            {
+                Text at = (Text)msg;
+                Console.WriteLine(at.messageCode);
+                Console.WriteLine(at.messageNumber);
+                Console.WriteLine(at.text);
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+            else {
+                Console.WriteLine("Nieudane typowanie obiektu.");
+            }
         }
     }
 }
