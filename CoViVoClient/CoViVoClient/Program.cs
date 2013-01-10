387c17aa65464293de23f4e8f58697bb6ea7da51
Tomasz Wiatrowski <daredeva@gmail.com>
2013-01-10 09:29:40 +0100

    podstawowy klient do testowania serwera

--- CoViVoClient/CoViVoClient/Program.cs
+++ CoViVoClient/CoViVoClient/Program.cs
@@ -1,21 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace CoViVoClient
 {
-    static class Program
+    namespace WindowsFormsApplication1
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static class Program
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CoViVo());
+            /// <summary>
+            /// The main entry point for the application.
+            /// </summary>
+            [STAThread]
+            static void Main()
+            {
+                Client client = new Client();
+                byte[] msg1 = Encoding.ASCII.GetBytes("Pierwsza wiadomosc");
+                byte[] msg2 = Encoding.ASCII.GetBytes("Druga wiadomosc");
+                byte[] msg3 = Encoding.ASCII.GetBytes("Trzecia wiadomosc");
+
+                if (client.connect()) {
+                    Console.WriteLine("Polaczono z serwerem");
+                }
+                else {
+                    Console.WriteLine("Blad przy polaczeniu");
+                }
+                if (client.send_message(msg1)) {
+                    Console.WriteLine("Wyslano pierwsza wiadomosc");
+                    while (true) { }
+                }
+                else {
+                    Console.WriteLine("Nie udalo sie wyslac wiadomosci");
+                }
+                return;
+                //Application.EnableVisualStyles();
+                //Application.SetCompatibleTextRenderingDefault(false);
+                //Application.Run(new CoViVo());
+            }
         }
     }
 }
